c63c3f450d8bca22c883c9a83de45d84e1554a57
user24565 <lisachenko04@gmail.com>
2025-04-21 18:14:59 +0300

    version_0.2.1

--- task-manager/src/app/app.routes.ts
+++ task-manager/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { UserManagementComponent } from "./admin/user-management.component";
+import { LoginComponent } from "./auth/login.component";
+import { RegisterComponent } from "./auth/register.component";
+import { ChartsComponent } from "./charts/charts.component";
+import { TaskFormComponent } from "./tasks/task-form.component";
+import { TaskListComponent } from "./tasks/task-list.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/new', component: TaskFormComponent },
+  { path: 'tasks/edit/:id', component: TaskFormComponent },
+  { path: 'dashboard', component: ChartsComponent },
+  { path: 'admin', component: UserManagementComponent },
+];
